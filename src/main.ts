5593776d7a7bf19c9064aa026ffa614120a9989d
Franco Bianco <fbianco@tritansoftware.com>
2025-05-22 13:07:49 -0300

    [App] Minor change

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // Configure class-validator to use NestJS's dependency injection
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
